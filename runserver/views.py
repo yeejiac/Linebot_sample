6b50088be86cc88bdab7d7ae986290f2ed05c594
Your Name <you@example.com>
2020-12-03 20:55:10 +0800

    fix func

--- runserver/views.py
+++ runserver/views.py
@@ -30,11 +30,7 @@ def callback(request):
                     if event.message.type == 'location':
                         print("HI")
                         reply_text = str(event.message.latitude) + "," + str(event.message.longitude)
-                        message = {
-                            'type': 'text',
-                            'text': reply_text
-                        } 
-                        line_bot_api.reply_message( event.reply_token, json.loads(message))         
+                        line_bot_api.reply_message( event.reply_token, TextSendMessage(text=reply_text))         
                     else:
                         line_bot_api.reply_message( event.reply_token, TextSendMessage(text=event.message.text))
                 else:
