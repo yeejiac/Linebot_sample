dca477941999e12d1fee23f1590e2c0a6fff98d1
Your Name <you@example.com>
2020-12-05 19:02:14 +0800

    fix get_nearby_restaurant func

--- runserver/locationTransfer.py
+++ runserver/locationTransfer.py
@@ -6,7 +6,7 @@ sys.path.insert(0, parent_dir)
 from dotenv import load_dotenv, find_dotenv
 from os.path import join, dirname
 dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path, override=True)  # 設定 override 才會更新變數哦！
+load_dotenv(dotenv_path, override=True)
 
 def get_nearby_restaurant(lon, lat):
     GOOGLE_PLACES_API_KEY = os.environ.get("GOOGLE_PLACES_API_KEY")
@@ -15,8 +15,8 @@ def get_nearby_restaurant(lon, lat):
     radius = 500
     place_type = 'restaurant'
     places_radar_result = gmaps.places_nearby(location, radius, type=place_type)
-    urlstring = ['https://www.google.com/maps/place/?q=place_id:'+i['place_id'] for i in places_radar_result['results']]
-    # print(urlstring)
+    urlstring = ['https://www.google.com/maps/place/?q=place_id:'+i['place_id'] for i in places_radar_result['results'] if float(i['rating']) > 4 and i['opening_hours']['open_now']==True]
+    print(urlstring)
     return urlstring
 
 
