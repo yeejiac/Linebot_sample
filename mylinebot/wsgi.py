f5515a0fc515d818ac6dde0b5e27c5543df15233
Your Name <you@example.com>
2021-01-05 22:57:55 +0800

    fh1

--- mylinebot/wsgi.py
+++ mylinebot/wsgi.py
@@ -6,8 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-
 import os
+import sys
+parent_dir = os.path.dirname(sys.path[0])
+sys.path.insert(0, parent_dir)
 
 from django.core.wsgi import get_wsgi_application
 
