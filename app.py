2d88b1efe5f0eec1b3178c88a8f7081b1fdb06ba
Your Name <you@example.com>
2021-01-09 16:18:44 +0800

    ipdate requirement file

--- app.py
+++ app.py
@@ -25,38 +25,37 @@ def handle_text(event):
         line_bot_api.reply_message( event.reply_token, TextSendMessage(text=event.message.text))
 
 
-@app.route("/mylinebot/callback", methods=['POST','GET'])
+@app.route("/mylinebot/callback", methods=['POST'])
 def callback(request):
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
         try:
             events = parser.parse(body, signature)  # 傳入的事件
+            for event in events:
+                if isinstance(event, MessageEvent):  # 如果有訊息事件
+                    print(event)
+                    if event.type == "message":
+                        if event.message.type == 'location':
+                            urlList = get_nearby_restaurant(str(event.message.latitude), str(event.message.longitude))
+                            if not urlList:
+                                line_bot_api.reply_message( event.reply_token, TextSendMessage(text='error happen'))
+                            else:
+                                if len(urlList) == 0:
+                                    line_bot_api.reply_message( event.reply_token, TextSendMessage(text='error happen'))
+                                elif len(urlList) < 5:
+                                    line_bot_api.reply_message( event.reply_token, [TextSendMessage(text= i) for i in urlList])
+                                else:
+                                    line_bot_api.reply_message( event.reply_token, [TextSendMessage(text= i) for i in urlList[0:5]])
+                        if event.message.type == "text":
+                            handle_text(event)
+                            # data = [event.source.user_id, "", event.message.text]
+                        else:
+                            line_bot_api.reply_message( event.reply_token, TextSendMessage(text='我聽不懂'))
+                    else:
+                        print("un recognize message")
         except InvalidSignatureError:
             print("InvalidSignatureError error")
-        for event in events:
-            if isinstance(event, MessageEvent):  # 如果有訊息事件
-                print(event)
-                if event.type == "message":
-                    if event.message.type == 'location':
-                        urlList = get_nearby_restaurant(str(event.message.latitude), str(event.message.longitude))
-                        if not urlList:
-                            line_bot_api.reply_message( event.reply_token, TextSendMessage(text='error happen'))
-                        else:
-                            if len(urlList) == 0:
-                                line_bot_api.reply_message( event.reply_token, TextSendMessage(text='error happen'))
-                            elif len(urlList) < 5:
-                                line_bot_api.reply_message( event.reply_token, [TextSendMessage(text= i) for i in urlList])
-                            else:
-                                line_bot_api.reply_message( event.reply_token, [TextSendMessage(text= i) for i in urlList[0:5]])
-                    if event.message.type == "text":
-                        handle_text(event)
-                        # data = [event.source.user_id, "", event.message.text]
-                    else:
-                        line_bot_api.reply_message( event.reply_token, TextSendMessage(text='我聽不懂'))
-                else:
-                    print("un recognize message")
-        return "OK"
 
 
 if __name__ == '__main__':
