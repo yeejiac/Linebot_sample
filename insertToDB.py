0cefbd67d149a351d40c1bb7af07d88ecc6151ec
Your Name <you@example.com>
2021-01-09 20:05:53 +0800

    update md5 func

--- insertToDB.py
+++ insertToDB.py
@@ -7,12 +7,19 @@ import pandas as pd
 parent_dir = os.path.dirname(sys.path[0])
 sys.path.insert(0, parent_dir)
 import configparser
+import hashlib
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 LINE_CHANNEL_ACCESS_TOKEN = config['line_bot']['LINE_CHANNEL_ACCESS_TOKEN']
 
 disk_engine = create_engine('sqlite:///friend_storage.db')
+
+def getMD5(plaintext):
+    m = hashlib.md5()
+    m.update(plaintext.encode("utf-8"))
+    hashtext = m.hexdigest()
+    return hashtext
 
 def getUserProfile(userID):
     try:
@@ -28,6 +35,7 @@ def insertDB(dataList):
     dataList[1] = getUserProfile(dataList[0])
     now = datetime.now()
     dataList.append(now.strftime("%d/%m/%Y %H:%M:%S"))
+    dataList[0] = getMD5(dataList[0])
     print(dataList)
     df = pd.DataFrame([dataList], columns=['userID','userName', 'Text', 'dateTime'])
     df.to_sql('friend_info', disk_engine, if_exists='append')
@@ -37,8 +45,3 @@ def showTable(sql):
     df = pd.read_sql(sql, disk_engine)
     print(df)
     return df
-
-
-# if __name__ == '__main__':
-#     list1 = ['U0cbce8948218ce7322a6a2290f1a7f4d', "", "321"]
-#     insertDB(list1)
